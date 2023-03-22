6390b46a3705da00aa1a94c45488bd4b74125b6a
Sean Zweifel <spzweifel@gmail.com>
2023-03-21 20:53:37 -0400

    added time blocks and a clock

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  //make this reactive so it's updating in real time
+  var time = dayjs().format('HH:mm:ss');
+  $('#currentDay').text(time);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
